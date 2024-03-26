2b97eed2cb2c193cdad551b18fa61f6a8b647415
James <164978181+jamespairepo@users.noreply.github.com>
2024-03-25 20:57:14 -0700

    Delete text directory

--- text/ds/mckinney_pda/hw.py
+++
@@ -1,9 +0,0 @@
-print('hi   there')
-
-print('hello world test 2')
-print('hello world test 3')
-print('hello world test 4')
-print('does this copy back to vscode?') 
-x = 'what happens when i edit here?'
-print(x)
-print(x, 'again')
