c1f0b59a975686d4b7286881802014ed86a8634d
James <jamespairepo@gmail.com>
2024-04-04 16:33:43 -0700

    Create 0567_permutation_in_a_string.py

---
+++ challenge/leetcode/sliding_window/0567_permutation_in_a_string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+
+
+s1 = "ab" # True
+s2 = "eidbaooo"
+
+# Input: s1 = "ab", s2 = "eidboaoo"
+# Output: false
+
+sol = Solution()
+print(sol.checkInclusion(s1, s2))
