f537be256e376cffe8c9853254711af8e089050c
James <jamespairepo@gmail.com>
2024-04-04 22:35:47 -0700

    update

--- challenge/leetcode/sliding_window/0567_permutation_in_a_string.py
+++ challenge/leetcode/sliding_window/0567_permutation_in_a_string.py
@@ -1,12 +1,68 @@
+# class Solution:
+#     def checkInclusion(self, s1: str, s2: str) -> bool:
+
+#         l, r = 0, 0
+
+#         lists1 = list(s1)
+#         lists2 = list(s2)
+
+#         while l <= len(s2) - len(s1):
+#             if lists2[r] in lists1:
+#                 lists1.remove(lists2[r])
+#                 if len(lists1) == 0:
+#                     return True
+#                     break
+#                 else:
+#                     r += 1
+#             else:
+#                 lists1 = list(s1)
+#                 l += 1
+#                 r = l
+#         return False
+
+
+# s1 = "ab"  # True
+# s2 = "eidbaooo"
+
+# s1 = "ab" # False
+# s2 = "eidboaoo"
+
+# s1 = "adc" # True
+# s2 = "dcda"
+
+
+# Output: false
+
+# sol = Solution()
+# print(sol.checkInclusion(s1, s2))
+
+
+
+
+### try two 
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
+
+        while s1[0] in list(s2):
+            
 
 
-s1 = "ab" # True
-s2 = "eidbaooo"
 
-# Input: s1 = "ab", s2 = "eidboaoo"
+
+        return False
+
+
+# s1 = "ab"  # True
+# s2 = "eidbaooo"
+
+# s1 = "ab" # False
+# s2 = "eidboaoo"
+
+s1 = "adc" # True
+s2 = "dcda"
+
+
 # Output: false
 
 sol = Solution()
