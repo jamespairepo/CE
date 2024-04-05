825c7cd46030ee9439765b63e295b5ec979c63db
James <jamespairepo@gmail.com>
2024-04-04 22:43:54 -0700

    Update 0567_permutation_in_a_string.py

--- challenge/leetcode/sliding_window/0567_permutation_in_a_string.py
+++ challenge/leetcode/sliding_window/0567_permutation_in_a_string.py
@@ -37,18 +37,32 @@
 # print(sol.checkInclusion(s1, s2))
 
 
+### try two
 
-
-### try two 
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
         while s1[0] in list(s2):
-            
 
+            lists1 = list(s1)
+            lists2 = list(s2)
 
+            for letter in lists1[1:]:
 
+                l, r = 1, 1
+
+                if letter == lists2.index(s1[0] - l):
+                    l += 1
+                    continue
+
+                elif letter == lists2.index(s1[0] + r):
+                    r += 1
+                    continue
+
+                else:
+                    lists2.remove(s1[0])
+                    l, r = 1, 1
 
         return False
 
@@ -59,7 +73,7 @@ class Solution:
 # s1 = "ab" # False
 # s2 = "eidboaoo"
 
-s1 = "adc" # True
+s1 = "adc"  # True
 s2 = "dcda"
 
 
